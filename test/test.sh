32e95b9441d940312069c533c78f0dbb3f7a3d53
Mark Baaijens <mark.baaijens@gmail.com>
2020-11-27 12:40:35 +0100

    Variable for root-direcory

--- test/test.sh
+++ test/test.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-# Presume: flacs are in ./files/flac
+# Presume: flacs are in $root/flac
 # Note: all tests are idempotent
 
 function given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_filecount {
     echo "* ${FUNCNAME[0]}"
-    rm -f ./files/mtranscoder.log
-    rm -rf ./files/mp3/*
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --mp3folder ./files/mp3 --logfolder ./files/
-    if cat ./files/mtranscoder.log | grep -q "transcoded to mp3: 10"; then echo "(log) OK"; else echo "(log) Fail"; fi
-    if find ./files/mp3 -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
+    rm -f $root/mtranscoder.log
+    rm -rf $root/mp3/*
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --mp3folder $root/mp3 --logfolder $root/
+    if cat $root/mtranscoder.log | grep -q "transcoded to mp3: 10"; then echo "(log) OK"; else echo "(log) Fail"; fi
+    if find $root/mp3 -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
 
 function given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_filecount {
     echo "* ${FUNCNAME[0]}"
-    rm -f ./files/mtranscoder.log
-    rm -rf ./files/ogg/*
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --oggfolder ./files/ogg --logfolder ./files/
-    if cat ./files/mtranscoder.log | grep -q "transcoded to ogg: 10"; then echo "(log) OK"; else echo "(log) Fail"; fi
-    if find ./files/ogg -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
+    rm -f $root/mtranscoder.log
+    rm -rf $root/ogg/*
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --oggfolder $root/ogg --logfolder $root/
+    if cat $root/mtranscoder.log | grep -q "transcoded to ogg: 10"; then echo "(log) OK"; else echo "(log) Fail"; fi
+    if find $root/ogg -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
 
 function given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_filecount {
     echo "* ${FUNCNAME[0]}"
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --mp3folder ./files/mp3 --logfolder ./files/
-    rm -f ./files/mtranscoder.log
-    rm -rf "./files/mp3/Nightwish/Human II Nature/02 - Noise.mp3"
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --mp3folder ./files/mp3 --logfolder ./files/
-    if cat ./files/mtranscoder.log | grep -q "transcoded to mp3: 1"; then echo "(log) OK"; else echo "(log) Fail"; fi
-    if find ./files/mp3 -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --mp3folder $root/mp3 --logfolder $root/
+    rm -f $root/mtranscoder.log
+    rm -rf "$root/mp3/Nightwish/Human II Nature/02 - Noise.mp3"
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --mp3folder $root/mp3 --logfolder $root/
+    if cat $root/mtranscoder.log | grep -q "transcoded to mp3: 1"; then echo "(log) OK"; else echo "(log) Fail"; fi
+    if find $root/mp3 -type f | wc -l  | grep -q "13"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
 
 function given_all_files_when_remove_cover-file_and_transcode_then_correct_logfile {
     echo "* ${FUNCNAME[0]}"
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --mp3folder ./files/mp3 --logfolder ./files/
-    rm -f ./files/mtranscoder.log
-    rm -rf "./files/mp3/Nightwish/Human II Nature/cover.jpg"
-    python3 ~/source/transcoder/transcoder.py ./files/flac/ --mp3folder ./files/mp3 --logfolder ./files/
-    if cat ./files/mtranscoder.log | grep -q "embedding album art \[source_tree\]/Nightwish/Human II Nature/cover.jpg"; then echo "(log) OK"; else echo "(log) Fail"; fi
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --mp3folder $root/mp3 --logfolder $root/
+    rm -f $root/mtranscoder.log
+    rm -rf "$root/mp3/Nightwish/Human II Nature/cover.jpg"
+    python3 ~/source/transcoder/transcoder.py $root/flac/ --mp3folder $root/mp3 --logfolder $root/
+    if cat $root/mtranscoder.log | grep -q "embedding album art \[source_tree\]/Nightwish/Human II Nature/cover.jpg"; then echo "(log) OK"; else echo "(log) Fail"; fi
 }
+
+root="./files"
 
 given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_filecount
 given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_filecount
