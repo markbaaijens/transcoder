9b6c3068277db4853449f4740d130969e998f593
Mark Baaijens <mark.baaijens@gmail.com>
2020-12-14 17:59:16 +0100

    Added transcoding to ogg

--- test/run.sh
+++ test/run.sh
@@ -2,4 +2,4 @@
 # Presume: flacs are in $root/flac
 
 root="./files"
-python3 ../transcoder.py --verbose $root/flac/ --mp3folder $root/mp3 
+python3 ../transcoder.py --verbose $root/flac/ --mp3folder $root/mp3 --oggfolder $root/ogg
