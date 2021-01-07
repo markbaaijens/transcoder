800450aaee69d772a9cf3af74cf6e86783cc4897
dartello <dartello@gmail.com>
2021-01-07 21:56:22 +0100

    refactor

--- test/test.sh
+++ test/test.sh
@@ -2,45 +2,45 @@
 # Presume: flacs are in $root/flac
 # Note: all tests are idempotent
 
-function given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_filecount {
+function given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_mp3_filecount {
     echo "* ${FUNCNAME[0]}"
     source="$root/flac"
     destination="$root/mp3"
+    python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
     rm -f $root/$log_file
+
     rm -rf $destination/*
     python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
+
     if cat $root/$log_file | grep -q "transcoded to mp3: $(find $source -type f -name "*.flac" | wc -l)"; then echo "(log) OK"; else echo "(log) Fail"; fi
     if find $destination -type f -name "*.mp3" | wc -l  | grep -q "$(find $source -type f -name "*.flac" | wc -l)"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
 
-function given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_filecount {
+function given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_ogg_filecount {
     echo "* ${FUNCNAME[0]}"
     source="$root/flac"
     destination="$root/ogg"
+    python3 ../transcoder.py $source --oggfolder $destination --logfolder $root/
     rm -f $root/$log_file
+
     rm -rf $destination/*
     python3 ../transcoder.py $source --oggfolder $destination --logfolder $root/
+    
     if cat $root/$log_file | grep -q "transcoded to ogg: $(find $source -type f -name "*.flac" | wc -l)"; then echo "(log) OK"; else echo "(log) Fail"; fi
     if find $destination -type f -name "*.ogg" | wc -l  | grep -q "$(find $source -type f -name "*.flac" | wc -l)"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
 
-function given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_filecount {
+function given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_mp3_filecount {
     echo "* ${FUNCNAME[0]}"
     source="$root/flac"
     destination="$root/mp3"
     python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
-    
-    count=$(find $destination -type f -name "*.mp3" | wc -l)
-    if [[ $count -le 0 ]] ; then echo "No file to remove"; return 1; fi
-    rand=$(($RANDOM % $count))
-    readarray -d '' flacs < <(find $destination -type f -name "*.mp3" -print0)
     rm -f $root/$log_file
 
-    # Picks a random MP3-file from the list of known files
-    rm -rf "${flacs[$rand]}"
-    echo "\"${flacs[$rand]}\" removed"
-    
+    readarray -d '' flacs < <(find $destination -type f -name "*.mp3" -print0)
+    rm -rf "${flacs[0]}"
     python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
+    
     if cat $root/$log_file | grep -q "transcoded to mp3: 1"; then echo "(log) OK"; else echo "(log) Fail"; fi
     if find $destination -type f -name "*.mp3" | wc -l  | grep -q "$(find $source -type f -name "*.flac" | wc -l)"; then echo "(count) OK"; else echo "(count) Fail"; fi
 }
@@ -51,27 +51,20 @@ function given_all_files_when_remove_cover-file_and_transcode_then_correct_logfi
     destination="$root/mp3"
     python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
     
-    count=$(find $destination -type f -name "cover.jpg" | wc -l)
-    if [[ $count -le 0 ]] ; then echo "No file to remove"; return 1; fi
-    rand=$(($RANDOM % $count))
     readarray -d '' images < <(find $destination -type f -name "cover.jpg" -print0)
     rm -f $root/$log_file
-
-    # Picks a random MP3-file from the list of known files
-    image="${images[$rand]}"
+    image="${images[0]}"
     rm -rf "$image"
-    echo "\"$image\" removed"
-    cover=${image/$destination/"\- copying to \[mp3_tree\]"}
-    echo "$cover"
-
     python3 ../transcoder.py $source --mp3folder $destination --logfolder $root/
-    if cat $root/$log_file | grep -q "$cover"; then echo "(log) OK"; else echo "(log) Fail"; fi
+
+    searchstringlog=${image/$destination/"\- copying to \[mp3_tree\]"}
+    if cat $root/$log_file | grep -q "$searchstringlog"; then echo "(log) OK"; else echo "(log) Fail"; fi
 }
 
 root="./files"
 log_file="transcoder.log"
 
-given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_filecount
-given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_filecount
-given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_filecount
+given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_mp3_filecount
+given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_ogg_filecount
+given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_mp3_filecount
 given_all_files_when_remove_cover-file_and_transcode_then_correct_logfile
