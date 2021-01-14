a8afbf8e606a0b109174633d206c35836e2deada
dartello <dartello@gmail.com>
2021-01-14 16:51:28 +0100

    get tags of flac and ogg

--- test/test.sh
+++ test/test.sh
@@ -121,14 +121,39 @@ function given_all_files_when_create_folders_in_lossy_tree_and_transcode_then_de
     if cat $root/$log_file | grep -q "\- empty folders deleted: 3"; then echo "(log) OK"; else echo "(log) Fail"; fi
 }
 
+function given_all_files_when_delete_lossy_file_and_transcode_then_check_if_tags_in_mp3_and_ogg_are_the_same_as_in_source
+{
+    echo "* ${FUNCNAME[0]}"
+    source="$root/flac"
+    python3 ../transcoder.py $source --mp3folder $root/mp3 --oggfolder $root/ogg --logfolder $root/
+    rm -f $root/$log_file
+
+    readarray -d '' flacs < <(find $source -type f -name "*.flac" -print0)
+    # To replace space with space-escape, add: -e "s/ /\\\\ /g"
+    mp3_file=$(echo "${flacs[0]}" | sed -e "s/\/flac\//\/mp3\//g" -e "s/\.flac$/.mp3/g") 
+    ogg_file=$(echo "${flacs[0]}" | sed -e "s/\/flac\//\/ogg\//g" -e "s/\.flac$/.ogg/g")
+
+    rm -f $mp3_file
+    rm -f $ogg_file
+
+    python3 ../transcoder.py $source --mp3folder $root/mp3 --oggfolder $root/ogg --logfolder $root/
+    metaflac --export-tags-to=flac-tags.txt "${flacs[0]}"
+    echo "FLAC"
+    echo $(cat flac-tags.txt)
+    echo "OGG"
+    ogg_tags=$(vorbiscomment -l "$ogg_file")
+    echo ${ogg_tags%metadata*}
+}
+
 root="./files"
 log_file="transcoder.log"
 
-given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_mp3_filecount
-given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_ogg_filecount
-given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_mp3_filecount
-given_all_files_when_remove_cover-file_and_transcode_then_correct_logfile
-given_all_files_when_changed_to_newer_date_of_flac_then_retranscode
-given_all_files_when_changed_to_newer_date_of_source_cover-jpg_then_re-embed
-given_all_files_when_delete_source_file_then_delete_lossy_file
-given_all_files_when_create_folders_in_lossy_tree_and_transcode_then_delete_lossy_folders
+#given_all_files_when_remove_all_mp3-files_and_transcode_then_correct_logfile_and_mp3_filecount
+#given_all_files_when_remove_all_ogg-files_and_transcode_then_correct_logfile_and_ogg_filecount
+#given_all_files_when_remove_one_mp3-file_and_transcode_then_correct_logfile_and_mp3_filecount
+#given_all_files_when_remove_cover-file_and_transcode_then_correct_logfile
+#given_all_files_when_changed_to_newer_date_of_flac_then_retranscode
+#given_all_files_when_changed_to_newer_date_of_source_cover-jpg_then_re-embed
+#given_all_files_when_delete_source_file_then_delete_lossy_file
+#given_all_files_when_create_folders_in_lossy_tree_and_transcode_then_delete_lossy_folders
+given_all_files_when_delete_lossy_file_and_transcode_then_check_if_tags_in_mp3_and_ogg_are_the_same_as_in_source
