59230d17ca177a1c628b76bc0b8deb51094a7de5
Mike P.J. van Dartel <60674543+dartello@users.noreply.github.com>
2021-01-14 22:12:31 +0100

    Issue 2 test 5 given all files when delete lossy file and transcode then check if tags in mp3 and ogg are the same as in source (#25)
    * get tags of flac and ogg
    
    * compare the title tag
    
    * compare the title tag with mp3
    
    * compare all nesseccary tags of ogg and mp3
    
    * remove comment

--- test/test.sh
+++ test/test.sh
@@ -121,6 +121,55 @@ function given_all_files_when_create_folders_in_lossy_tree_and_transcode_then_de
     if cat $root/$log_file | grep -q "\- empty folders deleted: 3"; then echo "(log) OK"; else echo "(log) Fail"; fi
 }
 
+function given_all_files_when_delete_lossy_file_and_transcode_then_check_if_tags_in_mp3_and_ogg_are_the_same_as_in_source
+{
+    echo "* ${FUNCNAME[0]}"
+    source="$root/flac"
+    python3 ../transcoder.py $source --mp3folder $root/mp3 --oggfolder $root/ogg --logfolder $root/
+    rm -f $root/$log_file
+
+    readarray -d '' flacs < <(find $source -type f -name "*.flac" -print0)
+    mp3_file=$(echo "${flacs[0]}" | sed -e "s/\/flac\//\/mp3\//g" -e "s/\.flac$/.mp3/g") 
+    ogg_file=$(echo "${flacs[0]}" | sed -e "s/\/flac\//\/ogg\//g" -e "s/\.flac$/.ogg/g")
+
+    rm -f $mp3_file
+    rm -f $ogg_file
+    python3 ../transcoder.py $source --mp3folder $root/mp3 --oggfolder $root/ogg --logfolder $root/
+
+    failure=0
+    flac_tag=$(metaflac "${flacs[0]}" --show-tag=TITLE | cut -d"=" -f 2)
+    ogg_tag=$(vorbiscomment -l "$ogg_file" | grep "TITLE" | cut -d"=" -f 2)
+    mp3_tag=$(ffprobe "$mp3_file" -show_entries format_tags=title -of compact=p=0:nk=1 -v 0)
+    if [[ "$flac_tag" != "$ogg_tag" ]]; then failure=1; fi
+    if [[ "$flac_tag" != "$mp3_tag" ]]; then failure=1; fi
+
+    flac_tag=$(metaflac "${flacs[0]}" --show-tag=ARTIST | cut -d"=" -f 2)
+    ogg_tag=$(vorbiscomment -l "$ogg_file" | grep "ARTIST" | cut -d"=" -f 2)
+    mp3_tag=$(ffprobe "$mp3_file" -show_entries format_tags=artist -of compact=p=0:nk=1 -v 0)
+    if [[ "$flac_tag" != "$ogg_tag" ]]; then failure=1; fi
+    if [[ "$flac_tag" != "$mp3_tag" ]]; then failure=1; fi
+    
+    flac_tag=$(metaflac "${flacs[0]}" --show-tag=ALBUM | cut -d"=" -f 2)
+    ogg_tag=$(vorbiscomment -l "$ogg_file" | grep "ALBUM" | cut -d"=" -f 2)
+    mp3_tag=$(ffprobe "$mp3_file" -show_entries format_tags=album -of compact=p=0:nk=1 -v 0)
+    if [[ "$flac_tag" != "$ogg_tag" ]]; then failure=1; fi
+    if [[ "$flac_tag" != "$mp3_tag" ]]; then failure=1; fi
+    
+    flac_tag=$(metaflac "${flacs[0]}" --show-tag=DATE | cut -d"=" -f 2)
+    ogg_tag=$(vorbiscomment -l "$ogg_file" | grep "DATE" | cut -d"=" -f 2)
+    mp3_tag=$(ffprobe "$mp3_file" -show_entries format_tags=date -of compact=p=0:nk=1 -v 0)
+    if [[ "$flac_tag" != "$ogg_tag" ]]; then failure=1; fi
+    if [[ "$flac_tag" != "$mp3_tag" ]]; then failure=1; fi
+    
+    flac_tag=$(metaflac "${flacs[0]}" --show-tag=TRACKNUMBER | cut -d"=" -f 2)
+    ogg_tag=$(vorbiscomment -l "$ogg_file" | grep "TRACKNUMBER" | cut -d"=" -f 2)
+    mp3_tag=$(ffprobe "$mp3_file" -show_entries format_tags=track -of compact=p=0:nk=1 -v 0)
+    if [[ "$flac_tag" != "$ogg_tag" ]]; then failure=1; fi
+    if [[ "$flac_tag" != "$mp3_tag" ]]; then failure=1; fi
+
+    if [[ $failure == 0 ]]; then echo "(tags) OK"; else echo "(tags) Fail"; fi
+}
+
 root="./files"
 log_file="transcoder.log"
 
@@ -132,3 +181,4 @@ given_all_files_when_changed_to_newer_date_of_flac_then_retranscode
 given_all_files_when_changed_to_newer_date_of_source_cover-jpg_then_re-embed
 given_all_files_when_delete_source_file_then_delete_lossy_file
 given_all_files_when_create_folders_in_lossy_tree_and_transcode_then_delete_lossy_folders
+given_all_files_when_delete_lossy_file_and_transcode_then_check_if_tags_in_mp3_and_ogg_are_the_same_as_in_source
